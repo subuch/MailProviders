3a7699e5ac6b0e9ad5af40e0dbe6fa0839a52621
subuch <subu.ramaoorthy@gmail.com>
2017-11-10 11:18:32 +1100

    Fix-v3-ClientSide Fix

--- EmailProviders/wwwroot/Email/Scripts/Controller/RouteController.js
+++ EmailProviders/wwwroot/Email/Scripts/Controller/RouteController.js
@@ -1,7 +1,41 @@
 ﻿var mailCtrl = function ($scope) {
-
+    $scope.Test = "Mail Service Providers";
 }
 
-var mailInfo = function ($scope) {
+var sendCtrl = function ($scope) {
+    $scope.Test = "Mail Drafting";
 
+    $scope.SendEmail = function (APIServiceFactory) {
+        console.log(APIServiceFactory);
+    if ($scope.to != undefined) {
+        emailObject = {
+            "from": $scope.from,
+            "to": $scope.to,
+            "cc": $scope.cc,
+            "bcc": $scope.bcc,
+            "subject": $scope.subject,
+            "text": $scope.text          
+        }
+        if (emailObject != null) {
+            var result = APIServiceFactory.getAPICallsWithModel("SendEmail", emailObject)
+                .then(function (response) {
+                    console.log("Data received at the client" + response.data);
+                    if (response.status == 200) {
+                        $scope.ReturnValue = response.data;                        
+                        $scope.showError = false;
+
+                    }
+                    else {
+                        $scope.showError = true;                        
+                    }
+                });
+
+        }
+    }
+    else {
+        alert("Enter atleast main Recipient email");
+        return;
+    }
 }
+}
+
--- EmailProviders/wwwroot/Email/Scripts/route.js
+++ EmailProviders/wwwroot/Email/Scripts/route.js
@@ -5,8 +5,8 @@
 var module = angular.module("moduleregistration", ["ngRoute"]);
 
 /********************Registration************************/
-module.controller("MailCtrl", mailCtrl);
-module.controller("MailInfoCtrl", mailInfo);
+module.controller("MailInfoCtrl", mailCtrl);
+module.controller("SendMailCtrl", sendCtrl);
 /*******************Utility************************/
 //module.directive("myCustomDirective", function () {
 //    return {
@@ -19,20 +19,57 @@ module.config(function ($routeProvider, $locationProvider) {
     $routeProvider
         .when("/Home", {
             templateUrl: "templates/Mail.html",
-            controller: "MailCtrl"
-        })
-
-        .when("/Info", {
-            templateUrl: "templates/MailInfo.html",
             controller: "MailInfoCtrl"
         })
 
+        .when("/Info", {
+            templateUrl: "templates/SendMail.html",
+            controller: "SendMailCtrl"
+        })
+
         .otherwise("/Home", {
-            templateUrl: "templates/student.html",
-            controller: "MailCtrl"
+            templateUrl: "templates/Mail.html",
+            controller: "MailInfoCtrl"
         })
     // $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('');
 
 });
+
+/*******RegisteredFactory******/
+module.factory("APIServiceFactory", ['$http', '$log', function ($http, $log) {
+
+
+    var returnObj = {};
+    returnObj.getAPICalls = function (actionName, attribute) {
+        return $http.get(serverURL + actionName + "/" + attribute)
+            .then(function (response) {
+                $log.info(response);
+                return response;
+            }, function (reason) {
+                $log.info(reason);
+                return reason;
+            })
+    }
+
+    returnObj.getAPICallsWithModel = function (method, objModel) {
+
+        return $http({
+            method: 'POST',
+            headers: { 'Content-Type': 'Application/JSON' },
+            data: objModel,
+            url: serverURL + method
+        })
+            .then(function (response) {
+                $log.info(response);
+                return response;
+            }, function (reason) {
+                $log.info(reason);
+                return reason;
+            });
+    }
+
+    return returnObj;
+}]);
+
 
